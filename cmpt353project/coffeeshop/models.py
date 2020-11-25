5eae2d1216fb8517a5f8ff19fbf1466b227ce73d
Ben <benredekop25@gmail.com>
2020-11-24 18:21:05 -0600

    got the database up and running and the admin site able to add items

--- cmpt353project/coffeeshop/models.py
+++ cmpt353project/coffeeshop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=55)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    items = models.ForeignKey(Item, on_delete=models.PROTECT)
+    price = models.FloatField()
+    ordered_at = models.DateTimeField('order_timestamp')
+
+    def __str__(self):
+        return self.items
